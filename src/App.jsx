c3615890d5c42981cbfe3d606772524f6a320f7d
BaltaKlixJones <balta98_@hotmail.com>
2023-06-07 14:14:21 -0300

    analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,12 @@ import Proyectos from './pages/Proyectos/Proyectos'
 import Contact from './pages/Contact/Contact'
 import Footer from './pages/Footer/Footer'
 import Proeloader from './pages/Preloader/Proeloader'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 function App() {
+
 
   return (
     <>
